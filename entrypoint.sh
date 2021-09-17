c2a5c154a7c613b979289a74c7fda22bbc739c0c
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-09-17 16:34:30 +0200

    use a random number for display on buffered x server

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,9 @@ INSECURE="${INSECURE:-"true"}"
 CONNECT_STRING="xfreerdp /v:${RDP_HOST} /u:${RDP_USER} /p:${RDP_PASSWORD} "
 if [[ ${INSECURE} == 'true' ]]; then CONNECT_STRING="${CONNECT_STRING} /cert:tofu"; fi
 
-Xvfb :1 &
-exec env DISPLAY=:1 ${CONNECT_STRING}
+# Create buffered x server
+DISPLAY_SEQ=$RANDOM
+Xvfb :${DISPLAY_SEQ} &
+
+# Run fake rdp within buffered x server
+exec env DISPLAY=:${DISPLAY_SEQ} ${CONNECT_STRING}
