f1c4ae1c457ca7961fcba6b943c505dc974b6f1d
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-09-17 12:51:56 +0200

    fix sh DEBUG interpretation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ ${DEBUG:-} = 'true' ]; then
+if [ "${DEBUG:-}" = "true" ]; then
     set -xuo 
 fi
 
 # connection string
-INSECURE=${INSECURE:-'true'}
+INSECURE="${INSECURE:-"true"}"
 CONNECT_STRING="xfreerdp /v:${RDP_HOST} /u:${RDP_USER} /p:${RDP_PASSWORD} "
 if [[ ${INSECURE} == 'true' ]]; then CONNECT_STRING="${CONNECT_STRING} /cert:tofu"; fi
 
