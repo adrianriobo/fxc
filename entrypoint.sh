1d25852544b33efc833dc30e02d782dc936475df
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-09-20 16:17:11 +0200

    test rdp client as non exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 INSECURE="${INSECURE:-"true"}"
 CONNECT_STRING="xfreerdp /v:${RDP_HOST} /u:${RDP_USER} /p:${RDP_PASSWORD} "
 if [[ ${INSECURE} == 'true' ]]; then CONNECT_STRING="${CONNECT_STRING} /cert:tofu "; fi
-CONNECT_STRING="${CONNECT_STRING} || true "
+# CONNECT_STRING="${CONNECT_STRING} || true "
 
 # Create buffered x server
 DISPLAY_SEQ=$RANDOM
@@ -19,4 +19,5 @@ while ! xdpyinfo -display :${DISPLAY_SEQ} >/dev/null 2>&1; do
 done
 
 # Run fake rdp within buffered x server
-exec env DISPLAY=:${DISPLAY_SEQ} ${CONNECT_STRING} 
+# exec env DISPLAY=:${DISPLAY_SEQ} ${CONNECT_STRING} 
+DISPLAY=:${DISPLAY_SEQ} ${CONNECT_STRING} 
